952120f716eb7ce9fcecd3bfeb6634108c671027
Valon Hajredini <hajredini.valon@gmail.com>
2016-09-11 22:56:23 +0200

    Added relation schip for notes and cards

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -3,4 +3,11 @@
 
 {!! $card->title!!}
 
+    <h2>My notes HEre</h2>
+    <ul>
+        @foreach($card->notes as $note)
+
+        <li>{!! $note->body  !!}</li>
+        @endforeach
+    </ul>
 @stop
